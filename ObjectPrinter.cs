2f8eb64d0fa63635caff143a192fbcbeee71374b
Hanna-Geffen <131370635+Hanna-Geffen@users.noreply.github.com>
2023-04-24 01:05:27 +0300

    Uploaded Object-Inspector Project

---
+++ ObjectPrinter.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Object_Inspector
+{
+    public interface ObjectVisitor
+    {
+        public void Primitive(Object obj);
+
+        public void ObjectStart(string name);
+
+        public void ObjectField(string name);
+
+        public void OnInfiniteLoop(string name);
+
+        public void ObjectEnd();
+
+        public void CollectionStart(bool isArray);
+
+        public void CollectionItem();
+
+        public void CollectionInterval(bool isLast);
+
+        public void CollectionEnd(bool isArray);
+
+        public void OnException(Exception e);
+    }
+
+    public class ConsoleObjectPrinter : ObjectVisitor
+    {
+        private string indent = "";
+
+        public void Primitive(Object obj)
+        {
+            Console.WriteLine(obj == null ? "null" : obj);
+        }
+
+        public void ObjectStart(string name)
+        {
+            Console.WriteLine("Object of Class \"" + name + "\"");
+            Console.WriteLine(indent + "------------------------");
+            IncrementIndent();
+        }
+
+        public void ObjectField(string name)
+        {
+            Console.Write(indent + name + " = ");
+        }
+
+        public void ObjectEnd()
+        {
+            DecrementIndent();
+        }
+
+        public void CollectionStart(bool isArray)
+        {
+            IncrementIndent();
+            Console.WriteLine(isArray ? "[" : "{");
+        }
+
+        public void CollectionItem()
+        {
+            Console.Write(indent);
+        }
+
+        public void CollectionInterval(bool isLast)
+        {
+
+        }
+
+        public void CollectionEnd(bool isArray)
+        {
+            DecrementIndent();
+            Console.Write(indent);
+            Console.WriteLine(isArray ? "]" : "}");
+        }
+
+        public void OnException(Exception e)
+        {
+            Console.WriteLine("Something went wrong...\n" + e);
+        }
+
+        public void OnInfiniteLoop(string name)
+        {
+            Console.WriteLine("Infinite loop detected.");
+            Console.WriteLine(indent + "Object of Class \"" + name + "\"");
+            Console.WriteLine(indent + "------------------------");
+        }
+
+        private void IncrementIndent()
+        {
+            indent += ".\t";
+        }
+
+        private void DecrementIndent()
+        {
+            indent = indent.Remove(indent.Length - 2);
+        }
+    }
+
+    public class ObjectInspector
+    {
+        private ObjectVisitor visitor;
+        HashSet<int> ObjectSet = new HashSet<int>();
+
+        public ObjectInspector(ObjectVisitor visitor)
+        {
+            this.visitor = visitor;
+        }
+
+        public void Accept(Object obj)
+        {
+            try
+            {
+                if (obj == null)
+                {
+                    visitor.Primitive(null);
+                    return;
+                }
+
+                Type type = obj.GetType();
+
+                if (IsPrimitive(type))
+                {
+                    visitor.Primitive(obj);
+                }
+                else if (IsCollection(type))
+                {
+                    visitor.CollectionStart(type.IsArray);
+
+                    IEnumerable collection = (IEnumerable)obj;
+
+                    int collectionLength = 0;
+                    foreach (object item in collection)
+                    {
+                        collectionLength++;
+                    }
+
+                    bool isLast = false;
+                    int i = 0;
+                    foreach (object item in collection)
+                    {
+                        visitor.CollectionItem();
+                        Accept(item);
+                        if (i == collectionLength - 1)
+                        {
+                            isLast = true;
+                        }
+                        visitor.CollectionInterval(isLast);
+                        i++;
+                    }
+
+                    visitor.CollectionEnd(type.IsArray);
+                }
+                else
+                {
+                    if (ObjectSet.Contains(obj.GetHashCode()))
+                    {
+                        visitor.OnInfiniteLoop(type.Name);
+                        return;
+                    }
+
+                    ObjectSet.Add(obj.GetHashCode());
+
+                    visitor.ObjectStart(type.Name);
+
+                    FieldInfo[] fields = type.GetFields();
+
+                    foreach (FieldInfo field in fields)
+                    {
+                        visitor.ObjectField(field.Name);
+                        object fieldObject = field.GetValue(obj);
+                        Accept(fieldObject);
+                    }
+
+                    ObjectSet.Remove(obj.GetHashCode());
+                    visitor.ObjectEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                visitor.OnException(e);
+            }
+        }
+
+        private static bool IsPrimitive(Type type)
+        {
+            return type.IsPrimitive || type.IsEnum || type == typeof(string) || type == typeof(decimal);
+        }
+
+        private static bool IsCollection(Type type)
+        {
+            if (type.IsArray)
+            {
+                return true;
+            }
+
+            Type[] interfaces = type.GetInterfaces();
+
+            foreach (Type iface in interfaces)
+            {
+                if (iface.IsAssignableFrom(typeof(IEnumerable<>)))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ObjectPrinter.test.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Object_Inspector
+{
+    public class Name
+    {
+        public string firstName;
+        public string lastName;
+        public List<string> nicknames;
+    }
+
+    public class Person
+    {
+        public int age;
+        public Name name;
+        public int height;
+        public string[] hobies;
+        public Person[] friends;
+        public Queue dreams;
+    }
+
+    public enum Color
+    {
+        red,
+        yellow,
+        green
+    }
+
+    public class ObjectPrinterTests
+    {
+        private static void RunTest(string testName, object obj)
+        {
+            ConsoleObjectPrinter printer = new ConsoleObjectPrinter();
+            ObjectInspector inspector = new ObjectInspector(printer);
+
+            Console.WriteLine("Test: " + testName);
+            Console.WriteLine("===================");
+            inspector.Accept(obj);
+            Console.WriteLine();
+        }
+
+        private static void RunTests()
+        {
+            RunTest("null object", null);
+
+            RunTest("int", 1);
+
+            RunTest("string", "one");
+
+            RunTest("float", 1.01);
+
+            RunTest("bool", true);
+
+            RunTest("enum", Color.green);
+
+            RunTest("Boolean", new Boolean());
+
+            RunTest("int array", new int[] { 0, 1, 2, 3 });
+
+            RunTest("string array", new string[] { "one", "two", "three", "four" });
+
+            RunTest("string list", new List<string>() { "one", "two", "three", "four" });
+
+            RunTest("int matrix", new int[][] { new int[] { 0, 1, 2, 3 }, new int[] { 0, 1, 2, 3 }, new int[] { 0, 1, 2, 3 } });
+
+            Name n1 = new Name();
+            n1.firstName = "Sarah";
+            n1.lastName = "Cohen";
+            n1.nicknames = new List<string>() { "Sari", "Sarale" };
+
+            Person sarah = new Person();
+            sarah.age = 26;
+            sarah.name = n1;
+            sarah.height = 175;
+            sarah.hobies = new string[] { "dancing" };
+            sarah.friends = null;
+
+            Name n2 = new Name();
+            n2.firstName = "Bat-Sheva";
+            n2.lastName = "Ayoun";
+            n2.nicknames = new List<string>() { "Batshevi", "Batchu" };
+
+            Person batsheva = new Person();
+            batsheva.age = 24;
+            batsheva.name = n2;
+            batsheva.height = 160;
+            batsheva.hobies = null;
+            batsheva.friends = new Person[] { sarah };
+
+            Name n3 = new Name();
+            n3.firstName = "Hanna";
+            n3.lastName = "Geffen";
+            n3.nicknames = null;
+
+            RunTest("Name object", n1);
+
+            Person hanna = new Person();
+            hanna.age = 25;
+            hanna.name = n3;
+            hanna.height = 165;
+            hanna.hobies = new string[] { "art", "painting", "music" };
+            hanna.dreams = new Queue();
+            hanna.dreams.Enqueue("car");
+            hanna.dreams.Enqueue(3.14);
+            hanna.dreams.Enqueue(26000);
+            hanna.dreams.Enqueue("hybrid");
+            hanna.friends = new Person[] { sarah, batsheva };
+
+            RunTest("Person object", hanna);
+
+            RunTest("Person List", new List<Person>() { hanna, sarah, batsheva });
+
+            hanna.friends = new Person[] { hanna, hanna, hanna };
+
+            RunTest("Infinite Loop", hanna);
+        }
+
+        static void Main(string[] args)
+        {
+            RunTests();
+        }
+    }
+}
